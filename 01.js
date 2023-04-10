4ed529e68c75235358e9e333ef7884b411772af7
khadjaa <islomkhadja2001@icloud.com>
2023-04-10 09:55:53 +0300

    added tasks for students

--- 01.js
+++ 01.js
@@ -1,1 +1,91 @@
-alert('work')
+const students = [
+    {
+        id: 1,
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        id: 2,
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 90,
+    },
+    {
+        id: 3,
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        id: 4,
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    },
+    {
+        id: 5,
+        name: "Helen",
+        age: 20,
+        isMarried: false,
+        scores: 110
+    },
+    {
+        id: 6,
+        name: "Ann",
+        age: 20,
+        isMarried: false,
+        scores: 105
+    },
+];
+
+const user = {
+    id: 7,
+    name: "Rick",
+    age: 23,
+    isMarried: false,
+    scores: 135,
+}
+
+
+// //1. Вывести массив с именами, в котором все имена будут прописаны большими буквами
+// console.log(students.map())
+
+// // 2. Вывести данные студентов с баллами >= 100
+// console.log(students.filter())
+
+// //3. Сформируйте массив холостых студентов
+// console.log(students.filter())
+
+//4. Объект user добавить в массив
+// let copyStudents =
+// console.log(copyStudents)
+
+//5. Удалить студента "John"
+//console.log(students.filter())
+
+//6***. Ник женился и в списке нужно заменить его статус на true
+// console.log(students.map())
+
+
+//7. Проверка на четнотность или не четность, вывод: "Четный" или "Нечетный"
+// const evenOrOdd = (number) => {
+// }
+// console.log(const evenOrOdd(7))
+// console.log(const evenOrOdd(8))
+
+//8. Создайте функцию, делится ли число n на оба числа x и y без остатка, return true or false.
+
+// function isDivisible(n, x, y) {
+// }
+// console.log(isDivisible(3,3,4))
+// console.log(isDivisible(12,3,4))
+
+
+
+
+
