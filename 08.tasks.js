abf1ab1c036f3b1571d973b1bd9f8edf39c512ed
khadjaa <islomkhadja2001@icloud.com>
2023-06-13 13:02:01 +0300

    solved tapCodeTranslation

--- 08.tasks.js
+++ 08.tasks.js
@@ -330,17 +330,37 @@
 //   return char.repeat(text.length)
 // }
 
-function countDiceThrows(n) {
-  const dp = new Array(n + 1).fill(0);
-  dp[0] = 1;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= 6; j++) {
-      if (j <= i) {
-        dp[i] += dp[i - j];
-      }
-    }
-  }
-  return dp[n];
-}
+// function countDiceThrows(n) {
+//   const dp = new Array(n + 1).fill(0);
+//   dp[0] = 1;
+//   for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= 6; j++) {
+//       if (j <= i) {
+//         dp[i] += dp[i - j];
+//       }
+//     }
+//   }
+//   return dp[n];
+// }
+//
+// console.log(countDiceThrows(4)); // Output: 8
 
-console.log(countDiceThrows(4)); // Output: 8
+function tapCodeTranslation(text) {
+  const tapCodeTable = {
+    a: [1, 1], b: [1, 2], c: [1, 3], k: [1, 3], d: [1, 4], e: [1, 5],
+    f: [2, 1], g: [2, 2], h: [2, 3], i: [2, 4], j: [2, 5],
+    l: [3, 1], m: [3, 2], n: [3, 3], o: [3, 4], p: [3, 5],
+    q: [4, 1], r: [4, 2], s: [4, 3], t: [4, 4], u: [4, 5],
+    v: [5, 1], w: [5, 2], x: [5, 3], y: [5, 4], z: [5, 5]
+  };
+
+  const encodedText = [];
+
+  for (let i = 0; i < text.length; i++) {
+    const letter = text[i];
+    const [row, column] = tapCodeTable[letter];
+    encodedText.push('.'.repeat(row) + ' ' + '.'.repeat(column));
+  }
+
+  return encodedText.join(' ');
+}
