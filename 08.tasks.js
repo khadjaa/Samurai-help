b23eeaef22565f1422f3df3884c0fe37074fdde9
khadjaa <islomkhadja2001@icloud.com>
2023-06-13 23:07:22 +0300

    solved triangular

--- 08.tasks.js
+++ 08.tasks.js
@@ -345,22 +345,35 @@
 //
 // console.log(countDiceThrows(4)); // Output: 8
 
-function tapCodeTranslation(text) {
-  const tapCodeTable = {
-    a: [1, 1], b: [1, 2], c: [1, 3], k: [1, 3], d: [1, 4], e: [1, 5],
-    f: [2, 1], g: [2, 2], h: [2, 3], i: [2, 4], j: [2, 5],
-    l: [3, 1], m: [3, 2], n: [3, 3], o: [3, 4], p: [3, 5],
-    q: [4, 1], r: [4, 2], s: [4, 3], t: [4, 4], u: [4, 5],
-    v: [5, 1], w: [5, 2], x: [5, 3], y: [5, 4], z: [5, 5]
-  };
+// function tapCodeTranslation(text) {
+//   const tapCodeTable = {
+//     a: [1, 1], b: [1, 2], c: [1, 3], k: [1, 3], d: [1, 4], e: [1, 5],
+//     f: [2, 1], g: [2, 2], h: [2, 3], i: [2, 4], j: [2, 5],
+//     l: [3, 1], m: [3, 2], n: [3, 3], o: [3, 4], p: [3, 5],
+//     q: [4, 1], r: [4, 2], s: [4, 3], t: [4, 4], u: [4, 5],
+//     v: [5, 1], w: [5, 2], x: [5, 3], y: [5, 4], z: [5, 5]
+//   };
+//
+//   const encodedText = [];
+//
+//   for (let i = 0; i < text.length; i++) {
+//     const letter = text[i];
+//     const [row, column] = tapCodeTable[letter];
+//     encodedText.push('.'.repeat(row) + ' ' + '.'.repeat(column));
+//   }
+//
+//   return encodedText.join(' ');
+// }
 
-  const encodedText = [];
-
-  for (let i = 0; i < text.length; i++) {
-    const letter = text[i];
-    const [row, column] = tapCodeTable[letter];
-    encodedText.push('.'.repeat(row) + ' ' + '.'.repeat(column));
+function triangular( n ) {
+  if ( n <= 0) {
+    return 0
   }
-
-  return encodedText.join(' ');
+  let sum = 0
+  for (let i = n; i > 0; i--) {
+    sum += i
+  }
+  return sum
 }
+
+console.log(triangular(3));
