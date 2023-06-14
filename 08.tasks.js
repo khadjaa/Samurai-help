dc9e60eb52e52facc3e72a082cd13d5d499fc8c8
khadjaa <islomkhadja2001@icloud.com>
2023-06-14 10:59:24 +0300

    solved Consonant value

--- 08.tasks.js
+++ 08.tasks.js
@@ -378,16 +378,45 @@
 //
 // console.log(triangular(3));
 
-function xor(a, b) {
-  // TODO: Program Me
-  return a !== b;
+// function xor(a, b) {
+//   // TODO: Program Me
+//   return a !== b;
+// }
+//
+// console.log(xor(false, false))
+//
+// function sortByLength (array) {
+//   // Return an array containing the same strings, ordered from shortest to longest
+//   return array.sort((a, b) => a.length - b.length)
+// }
+//
+// console.log(sortByLength(["Beg", "Life", "I", "To"]))
+
+function solve(s) {
+    const alphabet = {
+        a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10,
+        k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19,
+        t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+    };
+
+    let maxCount = 0
+    let currentCount = 0
+
+    for (let i = 0; i < s.length; i++) {
+        if ('aeiou'.indexOf(s[i]) === -1) {
+            currentCount += alphabet[s[i]]
+            if (currentCount > maxCount) {
+                maxCount = currentCount
+            }
+        } else {
+            if (currentCount > maxCount) {
+                maxCount = currentCount
+            }
+            currentCount = 0
+        }
+    }
+
+    return maxCount
 }
 
-console.log(xor(false, false))
-
-function sortByLength (array) {
-  // Return an array containing the same strings, ordered from shortest to longest
-  return array.sort((a, b) => a.length - b.length)
-}
-
-console.log(sortByLength(["Beg", "Life", "I", "To"]))
+console.log(solve('ashovvvvvvvvvvvvvvvvvvvvvvvvvvvvv'))
