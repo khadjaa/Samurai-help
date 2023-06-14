bdf70b3a853ac607fdf111af8e3c4615a1fd57ed
khadjaa <islomkhadja2001@icloud.com>
2023-06-14 09:45:50 +0300

    solved sortByLength

--- 08.tasks.js
+++ 08.tasks.js
@@ -384,3 +384,10 @@ function xor(a, b) {
 }
 
 console.log(xor(false, false))
+
+function sortByLength (array) {
+  // Return an array containing the same strings, ordered from shortest to longest
+  return array.sort((a, b) => a.length - b.length)
+}
+
+console.log(sortByLength(["Beg", "Life", "I", "To"]))
