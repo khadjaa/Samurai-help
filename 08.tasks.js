c5c565ac3172968a091d751841ec542d01c5281c
khadjaa <islomkhadja2001@icloud.com>
2023-06-19 01:18:00 +0300

    solved sortArray

--- 08.tasks.js
+++ 08.tasks.js
@@ -516,3 +516,15 @@
 function plural(n) {
     return n !== 1
 }
+
+function sortArray(a1, a2) {
+    let arr = []
+    for (let i = 0; i < a1.length; i++) {
+        for(let j = 0; j < a2.length; j++) {
+            if (a2[j][0] === a1[i][0]) {
+                arr.push(a2[j])
+            }
+        }
+    }
+    return arr
+}
