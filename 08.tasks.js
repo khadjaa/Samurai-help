d895e1cb8b858d1f098bcc20c3ab422fc3eb37d9
khadjaa <islomkhadja2001@icloud.com>
2023-06-28 22:29:39 +0300

    fakeBin

--- 08.tasks.js
+++ 08.tasks.js
@@ -596,9 +596,15 @@
 // //
 // // console.log(sumMul(2, -9))
 
-function hero(bullets, dragons){
-//Get Coding!
-    return dragons * 2 <= bullets
+// function hero(bullets, dragons){
+// //Get Coding!
+//     return dragons * 2 <= bullets
+// }
+//
+// console.log(hero(10, 5))
+
+function fakeBin(x){
+    return x.toString().split('').map(el => el >= 5 ? '1' : '0').join('')
 }
 
-console.log(hero(10, 5))
+console.log(fakeBin(45385593107843568))
