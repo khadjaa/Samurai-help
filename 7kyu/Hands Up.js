0a25d6e93c5449653c175504dae0239f00a48bfb
khadjaa <islomkhadja2001@icloud.com>
2023-07-09 11:45:14 +0300

    raiseHands solved

---
+++ 7kyu/Hands Up.js
@@ -0,0 +1,9 @@
+function raiseHands(step) {
+    const result = [0, 0, 0];
+
+    result[0] = step % 3;
+    result[1] = Math.floor((step / 3) % 3);
+    result[2] = Math.floor((step / 9) % 3);
+
+    return result;
+}
